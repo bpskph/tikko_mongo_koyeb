13c7d45bd732b5e277d650953f882c2d0dfb6177
refonando <rnganteng98@gmail.com>
2026-04-25 10:17:53 +0700

    feat: up to koyeb

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-require('dotenv').config()
+require("dotenv").config();
 const Pepesan = require("pepesan");
+const { fetchLatestBaileysVersion } = require("@whiskeysockets/baileys");
+const qrcode = require("qrcode-terminal");
 const router = require("./router");
-const { ALLOWED_NUMBERS } = process.env;
+
+// Ambil variabel environment
+const { ALLOWED_NUMBERS, PORT, MONGODB_URI } = process.env;
 
 (async () => {
-    const config = {
-        allowedNumbers: ALLOWED_NUMBERS ? ALLOWED_NUMBERS.split(',') : null,
-        browserName: 'Dewakoding App'
-    }
-    const pepesan = Pepesan.init(router, config)
-    await pepesan.connect()
-})()
+  const { version } = await fetchLatestBaileysVersion();
+  
+  const config = {
+    allowedNumbers:
+      ALLOWED_NUMBERS && ALLOWED_NUMBERS.trim() !== ""
+        ? ALLOWED_NUMBERS.split(",")
+        : null,
+    browserName: "Tikko Bot",
+    version,
+    // --- KONFIGURASI DATABASE ---
+    sessionStorage: {
+      type: "mongodb",
+      uri: MONGODB_URI, // Ambil dari file .env atau env di Koyeb
+      collectionName: "whatsapp_session"
+    },
+    // ----------------------------
+    server: {
+      port: PORT || 3000,
+    },
+    onQR: (id, state) => {
+      if (state.qr) {
+        console.log("\n--- SCAN QR CODE DI BAWAH INI ---");
+        qrcode.generate(state.qr, { small: true });
+      }
+    },
+  };
+
+  const pepesan = Pepesan.init(router, config);
+  await pepesan.connect();
+})();
